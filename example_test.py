22cdc68eb11cd0b48b971dce3ec80dac44318b04
Kanade Bhuvaneshwar <bhuvaneshwar_k_kanade@whirlpool.com>
2023-07-07 20:03:14 +0530

    enabled console and disabled Rx & Tx

--- example_test.py
+++ example_test.py
@@ -41,5 +41,20 @@ def test_examples_blink(env, extra_data):
     verify_elf_sha256_embedding(dut)
 
 
+@ttfw_idf.idf_example_test(env_tag='Example_GENERIC')
+def test_examples_system_console(env, extra_data):
+    dut = env.get_dut('console_example', 'examples/system/console', app_config_name='history')
+    print("Using binary path: {}".format(dut.app.binary_path))
+    dut.start_app()
+    dut.expect("Command history enabled")
+    env.close_dut(dut.name)
+
+    dut = env.get_dut('console_example', 'examples/system/console', app_config_name='nohistory')
+    print("Using binary path: {}".format(dut.app.binary_path))
+    dut.start_app()
+    dut.expect("Command history disabled")
+
+
+
 if __name__ == '__main__':
     test_examples_blink()
